6bc53869dd35c60a3333c16132d2403c4f4d9db3
TheHRGreen <thehrgreen@gmail.com>
2024-04-23 09:24:48 +0100

    big changes draws three cards including major aranca now

--- cards.py
+++ cards.py
@@ -9,26 +9,48 @@ import random
 
 # defining minor arcana
 
-class Minor_Arcana_Card:
-    def __init__(self, suit, rank):
+class TarotCard:
+    def __init__(self, suit=None, rank=None, name=None, number=None):
         self.suit = suit
         self.rank = rank
-    
+        self.name = name
+        self.number = number
+
+
     def __str__(self):
-        return f"{self.rank} of {self.suit}"
+        if self.suit and self.rank:
+            return f"{self.rank} of {self.suit}" #minor arcana
+        elif self.name and self.number is not None:
+            return f"{self.number}: {self.name}:" #major arcana
+
 
 class deck:
     def __init__(self):
         self.cards = []
         self.populate()
-        self.drawn_cards = []
+        self.shuffle()
+        #self.drawn_cards = []
+
+
 
     def populate(self):
+        major_arcana_names = ["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor",
+            "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit",
+            "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance",
+            "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
+            "Judgement", "The World"
+        ]
+
+        for i, name in enumerate(major_arcana_names):
+            self.cards.append(TarotCard(name=name, number=i))
+
+
         suits = ["Cups", "Pentacles", "Swords","Wands"]
         ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Page", "Knight", "Queen", "King"]
         for suit in suits:
             for rank in ranks:
-                self.cards.append(Minor_Arcana_Card(suit,rank))
+                self.cards.append(TarotCard(suit=suit, rank=rank))
+
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -36,10 +58,18 @@ class deck:
     def draw_card(self):
         if len(self.cards) == 0:
             return None
-        card = random.choice(self.cards)
-        self.cards.remove(card)
-        self.drawn_cards.append(card)
-        return card
+        else:
+            return self.cards.pop()
+
+
+
+
+
+
+        ##card = random.choice(self.cards)
+        ##self.cards.remove(card)
+        ##self.drawn_cards.append(card)
+        ##return card
     
 
     
--- main.py
+++ main.py
@@ -4,12 +4,16 @@
 import random
 import cards
 
+
+
 def main():
     deck = cards.deck()
-    deck.shuffle()
-    drawn_card = deck.draw_card()
-    if drawn_card:
-        print("you drew", drawn_card)
-    else:
-        print("the deck is empty")
+    drawn_cards= []
+    for _ in range(3):
+        drawn_card = deck.draw_card()
+        if drawn_card:
+            print(f"you drew, {drawn_card}")
+            drawn_cards.append(str(cards))
+        else:
+            print("the deck is empty")
 main()
